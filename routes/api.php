331b82a47ec0809725e98e84ef1ed01ddc8fd7d4
shfwnz <ilhamdzaky2007@gmail.com>
2025-04-13 16:08:14 +0700

    fix(auth upate): user auth can update

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 Route::apiResource('/wisata', App\Http\Controllers\Api\WisataController::class);
 Route::apiResource('/kategori', App\Http\Controllers\Api\KategoriController::class);
 
-Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'registration']);
-Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'registration']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 }); 
 
-Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:api')->put('/profile', [AuthController::class, 'updateProfile']);
+Route::middleware('auth:api')->put('/password', [AuthController::class, 'updatePassword']);
